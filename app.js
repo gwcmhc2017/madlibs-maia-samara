1aa34505932e137601dac64324c2fd5a9f3e858f
mhcuser <mhcuser@n21-133.mtholyoke.edu>
2017-08-11 14:44:57 -0400

    mada a madlibs

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $('button').click(function(){
+    console.log('hello');
+    var zachafron= $('#zachafron').val();
+    var padj1= $('#padj1').val();
+    var n1= $('#n1').val();
+    var pn1= $('#pn1').val();
+    var n2= $('#n2').val();
+    var n3= $('#n3').val();
+    var v1= $('#v1').val();
+    var adj1= $('#adj1').val();
+    var n10= $('#n10').val();
+    var adj2= $('#adj2').val();
+    var n4= $('#n5').val();
+    var n5= $('#n6').val();
+    var n6= $('#n7').val();
+    var n7= $('#n8').val();
+    var n8= $('#n9').val();
+    var n9= $('#n10').val();
+
+
+    $('#result').append(zachafron + '1.smoking hot he is everything I ' + padj1 + ' my whole life<br/>');
+    $('#result').append('2.I would not mind spending my life with a ' + n1 + ' like him for the rest of my life.<br/>');
+    $('#result').append('3.Unfortunatunly there were no ' + pn1 + ' in the ' + n2 +'<br/>');
+    $('#result').append('4.the' + n3 + 'was not ballin');
+    $('#result').append('5.I thought this ' + n4 + ' was about italilan ' + v1 + ' in seattle and ,boy was I ' + adj1 + ' when a cartoon showed up at my ' + n10 );
+    $('#result').append('6 I dont ' + adj2 + ' it, I think you have to be ' + n5 + ' or ');
+  });
+});
